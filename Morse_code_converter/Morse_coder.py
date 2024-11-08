c0d46695bbc03987140647e57fe6ef3c78964251
Burak Bayram <burakbayram41@hotmail.com>
2024-11-08 18:45:02 +0300

    Morse coder put into a folder Space race data anlysis got started in a a folder

---
+++ Morse_code_converter/Morse_coder.py
@@ -0,0 +1,59 @@
+morse_code_dict = {
+    'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..',   'E': '.',     'F': '..-.',
+    'G': '--.',   'H': '....',  'I': '..',    'J': '.---',  'K': '-.-',   'L': '.-..',
+    'M': '--',    'N': '-.',    'O': '---',   'P': '.--.',  'Q': '--.-',  'R': '.-.',
+    'S': '...',   'T': '-',     'U': '..-',   'V': '...-',  'W': '.--',   'X': '-..-',
+    'Y': '-.--',  'Z': '--..'
+}
+
+sentence = input("Text to translate\n")
+
+words = sentence.split()
+
+morse_code = " / ".join(" ".join(map(lambda letter: morse_code_dict.get(letter.upper(), "!"),word)) for word in words)
+
+print(morse_code)
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
